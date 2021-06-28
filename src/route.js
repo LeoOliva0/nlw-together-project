ce1b7d5320109f371a0a9affca0528c36350d34d
Leo Oliva <cassio_oliva@hotmail.com>
2021-06-27 23:26:11 -0300

    server

---
+++ src/route.js
@@ -0,0 +1,10 @@
+const express = require ('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render("index"))
+route.get('/room', (req,res) => res.render("room"))
+route.get('/create-pass', (req,res) => res.render("create-pass"))
+route.post('/room/:room/:question/:action', (req, res) => res.render())
+
+module.exports = route
