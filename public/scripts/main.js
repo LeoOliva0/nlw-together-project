2606208375cba198ed9ac1e111b52457810c150a
Leo Oliva <cassio_oliva@hotmail.com>
2021-06-28 00:15:23 -0300

    source

---
+++ public/scripts/main.js
@@ -0,0 +1,35 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+//Pegar todos os botoões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click",handleClick)
+})
+
+
+
+/*Quando o botão delete for clicado ele abre a modal*/
+const deleteButton = document.querySelectorAll(".actions .delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+   
+    modalTitle.innerHTML= `${text} esta pergunta`
+    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML= `Sim, ${text.toLocaleLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add()
+    modal.open()
+}
