0fdbe948269b3d9ad7732792d3dc7a348a0ac2d7
Leo Oliva <cassio_oliva@hotmail.com>
2021-06-29 02:04:11 -0300

    Nlw #6 project

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,18 +6,18 @@ const modalTitle = document.querySelector('.modal h2')
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
-//Pegar todos os botoões que existe com a classe check
-const checkButtons = document.querySelectorAll(".actions a.check")
+
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check") 
 
 checkButtons.forEach(button => {
     //adicionar a escuta
-    button.addEventListener("click",handleClick)
+    button.addEventListener("click", handleClick)
 })
 
 
-
-/*Quando o botão delete for clicado ele abre a modal*/
-const deleteButton = document.querySelectorAll(".actions .delete")
+/*Quando o botão delete for clicado ele abre a modal */
+const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
     button.addEventListener("click", (event) => handleClick(event, false))
@@ -27,15 +27,17 @@ function handleClick(event, check = true){
     event.preventDefault()
     const text = check ? "Marcar como lida" : "Excluir"
     const slug = check ? "check" : "delete"
-    const roomId= document.querySelector("#room-id").dataset.id
+    const roomId = document.querySelector("#room-id").dataset.id
     const questionId = event.target.dataset.id
 
-    const form = document.querySelector(".modal.form")
-    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
-    
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
+
     modalTitle.innerHTML= `${text} esta pergunta`
     modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-    modalButton.innerHTML= `Sim, ${text.toLocaleLowerCase()}`
-    check ? modalButton.classList.remove("red") : modalButton.classList.add()
+    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    
+    //abrir modal
     modal.open()
 }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const express = require ('express')
+const express = require('express')
 const route = require('./route')
 const path = require('path')
 
@@ -10,9 +10,8 @@ server.use(express.static("public"))
 
 server.set('views', path.join(__dirname, 'views'))
 
+server.use(express.urlencoded({extended: true}))
+
 server.use(route)
 
 server.listen(3000, () => console.log("RODANDO"))
-
-
-
